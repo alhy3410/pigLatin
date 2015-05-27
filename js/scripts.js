a99203b77ca472862b411a64cf8ab5daac378e7b
Andrew Lee & Franz Knupfer <student@epicodus.com>
2015-05-27 11:59:41 -0700

    pigLatin program can now effectively translate a single word into pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,15 @@ var pigLatin = function(phrase) {
     return newPhrase;
   } else {
 
-    while((phraseSplit[0] !== "a") && (phraseSplit[0] !== "e") && (phraseSplit[0] !== "i") && (phraseSplit[0] !== "o") && (phraseSplit[0] !== "u")) {
-      var letterToShift = phraseSplit.shift();
-      phraseSplit.push(letterToShift);
-
+    while((phraseSplit[0] !== "a") && (phraseSplit[0] !== "e") && (phraseSplit[0] !== "i") && (phraseSplit[0] !== "o") && (phraseSplit[0] !== "u"))  {
+      if ((phraseSplit[0] === "q") && (phraseSplit[1] === "u")) {
+        phraseSplit.shift()
+        phraseSplit.shift()
+        phraseSplit.push("qu")
+      } else {
+        var letterToShift = phraseSplit.shift();
+        phraseSplit.push(letterToShift);
+          }
     }
     phraseSplit.push("ay");
     var newPhrase = phraseSplit.join("");
--- spec/specs.js
+++ spec/specs.js
@@ -10,4 +10,12 @@ describe('pigLatin', function() {
   it("will move consonants to the end of a word and add ay if the word begins with a consonant", function() {
     expect(pigLatin("chat")).to.equal("atchay");
   });
+
+  it("will move consonants to the end of a word and add ay if the word begins with a consonant", function() {
+    expect(pigLatin("chthonic")).to.equal("onicchthay");
+  });
+
+  it("will move consonants to the end of a word and add ay if the word begins with a consonant", function() {
+    expect(pigLatin("squeal")).to.equal("ealsquay");
+  });
 });
