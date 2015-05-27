fc790ee30f5d8f104709c82ba869164899ac70ca
Andrew Lee & Franz Knupfer <student@epicodus.com>
2015-05-27 13:56:33 -0700

    working pig latin web app that translates sentences

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-var pigLatin = function(phrase) {
+var pigLatinSentence = function(sentence) {
+  var sentenceDowncase = sentence.toLowerCase();
+  var newSentence = sentenceDowncase.split(" ");
+  var finalSentence = [];
+  newSentence.forEach(function(phrase) {
+    var word = pigLatinWord(phrase);
+    finalSentence.push(word);
+  });
+  var answer = finalSentence.join(" ")
+  return answer;
+};
+
+var pigLatinWord = function(phrase) {
   var phraseSplit = phrase.split("");
   if ((phraseSplit[0] === "a") || (phraseSplit[0] === "e") || (phraseSplit[0] === "i") || (phraseSplit[0] === "o") || (phraseSplit[0] === "u")) {
     phraseSplit.push("ay");
@@ -8,9 +20,9 @@ var pigLatin = function(phrase) {
 
     while((phraseSplit[0] !== "a") && (phraseSplit[0] !== "e") && (phraseSplit[0] !== "i") && (phraseSplit[0] !== "o") && (phraseSplit[0] !== "u"))  {
       if ((phraseSplit[0] === "q") && (phraseSplit[1] === "u")) {
-        phraseSplit.shift()
-        phraseSplit.shift()
-        phraseSplit.push("qu")
+        phraseSplit.shift();
+        phraseSplit.shift();
+        phraseSplit.push("qu");
       } else {
         var letterToShift = phraseSplit.shift();
         phraseSplit.push(letterToShift);
@@ -23,3 +35,16 @@ var pigLatin = function(phrase) {
   }
 
 };
+
+$(document).ready(function() {
+  $("form#sentence1").submit(function(event) {
+    var sentence = ($("input#sentence").val());
+    var result = pigLatinSentence(sentence);
+
+    $(".translation").text(result);
+
+    $("#result").show();
+
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,21 +1,29 @@
 describe('pigLatin', function() {
   it("will add the letters ay to the end of any word that begins with a vowel", function() {
-    expect(pigLatin("at")).to.equal("atay");
+    expect(pigLatinWord("at")).to.equal("atay");
   });
 
   it("will move consonants to the end of a word and add ay if the word begins with a consonant", function() {
-    expect(pigLatin("cat")).to.equal("atcay");
+    expect(pigLatinWord("cat")).to.equal("atcay");
   });
 
   it("will move consonants to the end of a word and add ay if the word begins with a consonant", function() {
-    expect(pigLatin("chat")).to.equal("atchay");
+    expect(pigLatinWord("chat")).to.equal("atchay");
   });
 
   it("will move consonants to the end of a word and add ay if the word begins with a consonant", function() {
-    expect(pigLatin("chthonic")).to.equal("onicchthay");
+    expect(pigLatinWord("chthonic")).to.equal("onicchthay");
   });
 
   it("will move consonants to the end of a word and add ay if the word begins with a consonant", function() {
-    expect(pigLatin("squeal")).to.equal("ealsquay");
+    expect(pigLatinWord("squeal")).to.equal("ealsquay");
+  });
+
+  it("will translate two words into Pig Latin", function() {
+    expect(pigLatinSentence("chthonic squeal")).to.equal("onicchthay ealsquay");
+  });
+
+  it("will translate sentence into Pig Latin", function() {
+    expect(pigLatinSentence("I am a student")).to.equal("iay amay aay udentstay");
   });
 });
